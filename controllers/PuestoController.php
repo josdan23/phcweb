9ef03d5320887db7684a941c43f4d8a924f0ddc8
Martin_Arias <martinariasutn@gmail.com>
2017-11-20 18:19:47 -0300

    Ahora se muestran los Puestos

--- controllers/PuestoController.php
+++ controllers/PuestoController.php
@@ -1,6 +1,7 @@
 <?php
 
-
+require __DIR__.'/../domain/Puesto.php';
+require __DIR__.'/../persistencia/EntidadBase.php';
 
 class PuestoController {
 
--- views/panelIzquierda.php
+++ views/panelIzquierda.php
@@ -3,7 +3,7 @@
 	<ul id="hidden_area_1" class="nav nav-pills nav-stacked">
   		<li id="Empleados" class="estado"><a href="index.php?page=EmpleadoView">Empleados</a></li>
   		<li id="Departamento" class=""><a href="index.php?page=DepartamentoView">Departamento</a></li>
-  		<li id="Puestos" class="estado"><a href="panelAdministrador.php?page=cambios&estado=realizado">Puestos</a></li>
+  		<li id="Puestos" class="estado"><a href="index.php?page=PuestoView">Puestos</a></li>
   		<li id="Requerimientos" class="estado"><a href="panelAdministrador.php?page=cambios&estado=cerrado">Requerimientos</a></li>
   		<li id="Niveles" class="estado"><a href="./panelAdministrador.php?page=historial">Niveles</a></li>
   		<li id="Tareas" class="estado"><a href="./panelAdministrador.php?page=informe">Tareas</a></li>
