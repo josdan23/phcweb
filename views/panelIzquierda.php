b026649298f13e8955c24b08073d286d6bf627fb
Daniel Yapura <danielyapura@gmail.com>
2017-12-03 01:45:00 -0300

    Se agregaron iconos al panel de la izquierda

--- views/panelIzquierda.php
+++ views/panelIzquierda.php
@@ -4,13 +4,13 @@
 	<p id="title_cambios" class="text-muted small">OPCIONES</p>
 
 	<ul class="nav nav-pills nav-stacked">
-  		<li><a href="principalView.php?page=EmpleadoView">Empleados</a></li>
-      <li><a href="principalView.php?page=DepartamentoView">Departamento</a></li>
-      <li><a href="principalView.php?page=PuestosView">Puestos</a></li>
-      <li class="active"><a href="principalView.php?page=RequerimientosView">Requerimientos</a></li>
-  		<li><a href="principalView.php?page=NivelPuestoView">Niveles Puestos</a></li>
-  		<li><a href="principalView.php?page=NivelDepartamentoView">Niveles Departamento</a></li>
-  		<li><a href="principalView.php?page=TareasView">Tareas</a></li>
+  		<li><a href="principalView.php?page=EmpleadoView"><span class="glyphicon glyphicon-user"></span> Empleados</a></li>
+      <li><a href="principalView.php?page=DepartamentoView"><span class="glyphicon glyphicon-th-list"></span> Departamento</a></li>
+      <li><a href="principalView.php?page=PuestosView"><span class="glyphicon glyphicon-lock"></span> Puestos</a></li>
+      <li class="active"><a href="principalView.php?page=RequerimientosView"><span class="glyphicon glyphicon-list-alt"></span> Requerimientos</a></li>
+  		<li><a href="principalView.php?page=NivelPuestoView"><span class="glyphicon glyphicon-sort-by-order"></span> Niveles Puestos</a></li>
+  		<li><a href="principalView.php?page=NivelDepartamentoView"><span class="glyphicon glyphicon-sort-by-attributes"></span> Niveles Departamento</a></li>
+  		<li><a href="principalView.php?page=TareasView"><span class="glyphicon glyphicon-tasks"></span> Tareas</a></li>
 	</ul>
 
   <br>
@@ -18,7 +18,7 @@
   <p id="title_cambios" class="text-muted small">SISTEMA</p>
 
   <ul class="nav nav-pills nav-stacked">
-      <li><a href="principalView.php?page=UsuarioView">Usuarios</a></li>
+      <li><a href="principalView.php?page=UsuarioView"><span class="glyphicon glyphicon-user"></span> Usuarios</a></li>
   </ul>
 
   <br>
@@ -26,11 +26,11 @@
   <p id="title_cambios" class="text-muted small">REPORTES</p>
 
   <ul class="nav nav-pills nav-stacked">
-      <li><a href="principalView.php?page=Consulta1View">Consulta 1</a></li>
-      <li><a href="principalView.php?page=Consulta2View">Consulta 2</a></li>
-      <li><a href="principalView.php?page=Consulta3View">Consulta 3</a></li>
-      <li><a href="principalView.php?page=Consulta4View">Consulta 4</a></li>
-      <li><a href="principalView.php?page=Consulta5View">Consulta 5</a></li>
+      <li><a href="principalView.php?page=Consulta1View"><span class="glyphicon glyphicon-file"></span> Consulta 1</a></li>
+      <li><a href="principalView.php?page=Consulta2View"><span class="glyphicon glyphicon-file"></span> Consulta 2</a></li>
+      <li><a href="principalView.php?page=Consulta3View"><span class="glyphicon glyphicon-file"></span> Consulta 3</a></li>
+      <li><a href="principalView.php?page=Consulta4View"><span class="glyphicon glyphicon-file"></span> Consulta 4</a></li>
+      <li><a href="principalView.php?page=Consulta5View"><span class="glyphicon glyphicon-file"></span> Consulta 5</a></li>
       
   </ul>
 
